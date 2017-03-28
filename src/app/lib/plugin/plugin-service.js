1ab1a6c303eb75accf60d7711d35c3278fe162c4
SebHeuze <heuze.seb@gmail.com>
2017-03-29 01:54:56 +0200

    Initial plugin loading

--- src/app/lib/plugin/plugin-service.js
+++ src/app/lib/plugin/plugin-service.js
@@ -3,5 +3,38 @@ import {ReplaySubject} from "rxjs/Rx";
 
 @Injectable()
 export class PluginService {
-    
+    constructor() {
+        this.plugins = [];
+
+        // Tracking change of plugin list
+        this.change = new ReplaySubject(1);
+        this.loadPlugins();
+    }
+
+    loadPlugins() {
+        System.import('/plugins.js').then((pluginsModule) => {
+            pluginsModule.default.forEach( (pluginUrl) =>
+                this.loadPlugin(pluginUrl)
+            );
+        });
+    }
+
+    loadPlugin(url) {
+        return System.import(url).then((pluginModule) => {
+            const Plugin = pluginModule.default;
+            const pluginData = {
+                url,
+                type: Plugin,
+                // Reading the meta data previously stored by the @Plugin
+                // decorator
+                config: Plugin._pluginConfig,
+                // Creates the plugin instance
+                instance: new Plugin()
+            };
+            this.plugins = this.plugins.concat([pluginData]);
+            this.change.next(this.plugins);
+        });
+    }
+
+
 }
